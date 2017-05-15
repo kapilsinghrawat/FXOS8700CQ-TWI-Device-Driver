91174c1ab963911ab44e7446ac4ad176b869fedc
Kapil Singh Rawat <kapilrawat1508@gmail>
2017-05-15 18:56:36 +0530

    Use blocking mode & add TWI_handler

--- FXOS8700CQ_TWI.c
+++ FXOS8700CQ_TWI.c
@@ -18,6 +18,33 @@
 
 const nrf_drv_twi_t p_twi = NRF_DRV_TWI_INSTANCE(0);
 
+
+/* Indicates if operation on TWI has ended. */
+volatile bool m_xfer_done = false;
+
+void twi_handler(nrf_drv_twi_evt_t const * p_event, void * p_context)
+{
+    switch (p_event->type)
+    {
+        case NRF_DRV_TWI_EVT_DONE:
+            if (p_event->xfer_desc.type == NRF_DRV_TWI_XFER_RX)
+            {
+				SEGGER_RTT_printf(0,"\nInside TWI_EVT_DONE %d",m_xfer_done);
+            }
+            m_xfer_done = true;
+            break;
+		case NRF_DRV_TWI_EVT_ADDRESS_NACK:
+            SEGGER_RTT_printf(0,"\nInside NRF_DRV_TWI_EVT_ADDRESS_NACK");
+            break;
+        case NRF_DRV_TWI_EVT_DATA_NACK:
+            SEGGER_RTT_printf(0,"\nInside NRF_DRV_TWI_EVT_DATA_NACK");
+            break;
+        default:
+			SEGGER_RTT_printf(0,"\nInside TWI_EVT_DEFAULT");
+            break;
+    }
+}
+
 /* initialize twi init function */	
 bool twi_init(void)
 {
@@ -28,7 +55,7 @@ bool twi_init(void)
 	p_twi_config.sda = TWI_SDA_PIN;
 	p_twi_config.frequency = NRF_TWI_FREQ_400K;
 	
-	err_code = nrf_drv_twi_init(&p_twi, &p_twi_config, NULL, NULL);
+	err_code = nrf_drv_twi_init(&p_twi, &p_twi_config, twi_handler, NULL);
 	
 	if(err_code != NRF_SUCCESS)	return false ;
 	
@@ -36,33 +63,39 @@ bool twi_init(void)
 	return true;
 }
 
-/* Write in a registerss */
+/* Write in register's */
 void FXOS8700CQ_TWI_register_write(uint8_t slave_addr, uint8_t reg_addr, uint8_t value)
 {
     ret_code_t ret_code;
     uint8_t tx_data[2]= {reg_addr, value};
-    ret_code = nrf_drv_twi_tx(&p_twi, slave_addr, tx_data, sizeof(tx_data), false);
+    m_xfer_done = false;
+		ret_code = nrf_drv_twi_tx(&p_twi, slave_addr, tx_data, sizeof(tx_data), false);
+		while (m_xfer_done == false);
 		//APP_ERROR_CHECK (ret_code);
 		#ifdef fxos_debug
 			SEGGER_RTT_printf(0, "\nWrite register error code:%x", ret_code);
 		#endif
 }
 
-/* Read register on slave */
+/* Read register's */
 void FXOS8700CQ_TWI_register_read(uint8_t slave_addr, uint8_t reg_addr, uint8_t *p_data, uint32_t bytes)
 {   
     ret_code_t ret_code;
-   	ret_code = nrf_drv_twi_tx(&p_twi, slave_addr, &reg_addr, 1, true);
+   	m_xfer_done = false;
+		ret_code = nrf_drv_twi_tx(&p_twi, slave_addr, &reg_addr, 1, true);
+		while (m_xfer_done == false);
 		#ifdef fxos_debug
 			SEGGER_RTT_printf(0, "\nRead register tx error code:%x", ret_code);
 		#endif
-		//APP_ERROR_CHECK (ret_code);
-    ret_code = nrf_drv_twi_rx(&p_twi, slave_addr, p_data, bytes);
+		APP_ERROR_CHECK (ret_code);
+		
+		m_xfer_done = false;
+		ret_code = nrf_drv_twi_rx(&p_twi, slave_addr, p_data, bytes);
+		while (m_xfer_done == false);
 		#ifdef fxos_debug
 		SEGGER_RTT_printf(0, "\nRead register rx error cdoe:%x", ret_code);
-		//SEGGER_RTT_printf(0, "\nRead register val:%d", pdata);
 		#endif		
-		//APP_ERROR_CHECK (ret_code);
+		APP_ERROR_CHECK (ret_code);
 }
 
 //Set FXOS stanby mode write 0x00 in register
@@ -73,7 +106,6 @@ void FXOS8700CQ_standby()
 	#ifdef fxos_debug
 	SEGGER_RTT_printf(0, "\nFXOS8700CQ_CTRL_REG1_VAL:%d", ctrl_reg1_val[0]);
 	#endif		
-	
 	FXOS8700CQ_TWI_register_write(FXOS8700CQ_SLAVE_ADDR, FXOS8700CQ_CTRL_REG1, FXOS8700CQ_STANDBY_MODE_VAL); 
 }
 
@@ -87,16 +119,12 @@ void FXOS8700CQ_normal()
 	#ifdef fxos_debug
 	SEGGER_RTT_printf(0, "\nFXOS8700CQ_CTRL_REG1_VAL:%d", ctrl_reg1_val[0]);
 	#endif		
-	
 }
 
-// Read status and the three channels of accelerometer and magnetometer data from FXOS8700CQ (13 bytes)
+// Read status and the three channels of accelerometer and magnetometer data from FXOS8700CQ 
 uint8_t FXOS8700CQ_TWI_acc_data_read(fxos_data_t *pAccData, fxos_data_t *pMagData)
 {
-	 
-
 	uint8_t   Buffer[6]; // read buffer
-	//Read FXOS8700CQ_READ_LEN=13 bytes (status byte and the six channels of data)
 	FXOS8700CQ_TWI_register_read(FXOS8700CQ_SLAVE_ADDR, FXOS8700CQ_OUT_X_MSB, &Buffer[0], sizeof(Buffer));
 	if(sizeof(Buffer) == 6) 		   
 	{
@@ -122,38 +150,30 @@ uint8_t FXOS8700CQ_TWI_acc_data_read(fxos_data_t *pAccData, fxos_data_t *pMagDat
 //init function
 bool FXOS8700CQ_TWI_init(void)
 {
-
 	uint8_t data;
 	uint8_t r_data[1];
-	
-	
 	if(!twi_init())	return false;			
 	
 	//Read WHOAMI register 
 	FXOS8700CQ_TWI_register_read(FXOS8700CQ_SLAVE_ADDR, FXOS8700CQ_WHO_AM_I, &r_data[0], 1);
-	
-	
 	data = r_data[0];
 	#ifdef fxos_debug
 	SEGGER_RTT_printf(0, "\nWHO_AM_I Read Value:%d", data);
 	#endif		
-		
 	//Compear WHOAMI register value 
 	if (data != FXOS8700CQ_WHO_AM_I_VAL)		return false;
-
-	//stand by mode 
+	
 	FXOS8700CQ_standby();	
    
-	//Write 0x1F in register FXOS8700CQ_CTRL_REG1 for reduce noice from over sampling.
+	//Set FXOS8700CQ_CTRL_REG1 to reduce noice from over sampling.
 	//FXOS8700CQ_TWI_register_write(FXOS8700CQ_SLAVE_ADDR, FXOS8700CQ_M_CTRL_REG1, FXOS8700CQ_M_CTRL_REG1_VAL); 
 		
-	//Write 0x1F in register FXOS8700CQ_M_CTRL_REG2 for magnetic reset etc
+	//Set FXOS8700CQ_M_CTRL_REG2 for magnetic reset etc
 	//FXOS8700CQ_TWI_register_write(FXOS8700CQ_SLAVE_ADDR, FXOS8700CQ_M_CTRL_REG2, FXOS8700CQ_M_CTRL_REG2_VAL); 
 	
-	//Write 0x01 in register FXOS8700CQ_XYZ_DATA_CFG for acc 4G range 
+	//Set FXOS8700CQ_XYZ_DATA_CFG register 4G range 
 	FXOS8700CQ_TWI_register_write(FXOS8700CQ_SLAVE_ADDR, FXOS8700CQ_XYZ_DATA_CFG, FXOS8700CQ_XYZ_DATA_CFG_VAL); 
 	
-	//normal mode  
 	FXOS8700CQ_normal();	
 	return true;
 }
